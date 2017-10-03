e0ce3c94b5e4ccd0b086a92581b31e911f6c4c8f
Avi <Avizc@users.noreply.github.com>
2017-10-02 23:53:42 -0400

    Removed classes, added small notes

--- fizzbuzz/ruby_fizzbuzz.rb
+++ fizzbuzz/ruby_fizzbuzz.rb
@@ -1,18 +1,15 @@
-class Fizzbuzz
-    def fizzbuzz_1
-        for i in 1...19 do
-            if(i%3==0&&i%5==0)
-                puts "fizzbuzz"
-            elsif(i%buzz==0)
-                puts "buzz"
-            elsif(i%fizz==0)
-                puts "fizz"
-            else
-                puts "#{i}"
-            end
+# This works but couldn't get classes working
+# To do: figure out classes and what not
+def fizzbuzz
+    for i in 1...19 do
+        if(i%3==0&&i%5==0)
+            puts "fizzbuzz"
+        elsif(i%buzz==0)
+            puts "buzz"
+        elsif(i%fizz==0)
+            puts "fizz"
+        else
+            puts "#{i}"
         end
     end
 end
-# if __FILE__ == $0
-
-# end
